47d0727588beff84985ae3efe71cd44267151912
Smygi <m.melbing@gmail.com>
2014-10-14 13:13:13 +0200

    Init test project

---
+++ MediatorTests/ComputerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediatorTests
+{
+    [TestClass]
+    public class ComputerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
